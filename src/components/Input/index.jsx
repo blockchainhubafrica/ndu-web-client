246c154c707277ca4f2a516a8bd88f57dc0f02f0
davien21 <chidiebereekennia@gmail.com>
2021-10-03 23:10:47 +0100

    --feat: add input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./input.module.css";
+
+function Input({ type, onClick, ...rest }) {
+  return (
+    <div className={styles.container}>
+      <input type={type} onClick={onClick} {...rest} />
+    </div>
+  );
+}
+
+export default Input;
