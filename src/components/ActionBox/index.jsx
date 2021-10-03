6cecbd185b0ecd56688a43f88e2ecb5d1596dc23
davien21 <chidiebereekennia@gmail.com>
2021-10-03 23:10:23 +0100

    --feat: add action box component

---
+++ src/components/ActionBox/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./action-box.module.css";
+
+function ActionBox({ image, title, imgWidth, imgHeight }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.image}>
+        <img
+          src={image}
+          alt=""
+          width={imgWidth || 100}
+          height={imgHeight || 100}
+        />
+      </div>
+      <div className={styles.content}>{title}</div>
+    </div>
+  );
+}
+
+export default ActionBox;
