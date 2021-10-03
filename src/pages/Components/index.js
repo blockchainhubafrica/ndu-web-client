03e9b71553cd3440886bd781f5b37b275c92b2cb
davien21 <chidiebereekennia@gmail.com>
2021-10-03 23:13:34 +0100

    --feat: add components page

--- src/pages/Components/index.js
+++ src/pages/Components/index.js
@@ -1,10 +1,38 @@
-import React from 'react';
-
+import React from "react";
+import Input from "./../../components/Input/index";
+import {
+  DrugsSvg,
+  FullLogoIcon,
+  HalfLogoIcon,
+  MicroChipSvg,
+} from "../../assets/";
+import ActionBox from "../../components/ActionBox";
 function Components(props) {
   return (
-    <div className="p-4">
-      Hello Components! ✌
-    </div>
+    <>
+      <div className="p-4">
+        <p className="mb-4">This is an Input box</p>
+        <Input type="text" placeHolder="Drug name" />
+      </div>
+      <div className="p-4 flex flex-wrap gap-7 ">
+        <div>
+          <p className="mb-4">This is the Full Logo</p>
+          <FullLogoIcon />
+        </div>
+        <div>
+          <p className="mb-4">This is the Half Logo</p>
+          <HalfLogoIcon />
+        </div>
+      </div>
+
+      <div className="p-4">
+        <p className="mb-4">These are the Action Boxes</p>
+        <div className="flex flex-wrap flex-1 gap-7">
+          <ActionBox title="Generate Hash" image={MicroChipSvg} />
+          <ActionBox title="Drug Inventory" image={DrugsSvg} />
+        </div>
+      </div>
+    </>
   );
 }
 
