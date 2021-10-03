cbfe216fc4418de5dcc41eb01d8bc2d7d94a13ae
davien21 <chidiebereekennia@gmail.com>
2021-10-03 23:14:34 +0100

    --feat: add logo svgs

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,4 +1,15 @@
 import { ReactComponent as WarningIcon } from "./warningIcon.svg";
 import { ReactComponent as SheildIcon } from "./sheildIcon.svg";
+import { ReactComponent as FullLogoIcon } from "./logo.svg";
+import { ReactComponent as HalfLogoIcon } from "./half-logo.svg";
+import MicroChipSvg from "./micro-chip.svg";
+import DrugsSvg from "./drugs.svg";
 
-export { WarningIcon, SheildIcon };
+export {
+  WarningIcon,
+  SheildIcon,
+  FullLogoIcon,
+  HalfLogoIcon,
+  MicroChipSvg,
+  DrugsSvg,
+};
