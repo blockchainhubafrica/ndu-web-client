9dcdf9260230267fb2a18900495a0218a324c1a2
davien21 <chidiebereekennia@gmail.com>
2021-10-05 05:02:26 +0100

    change globe stats png to clearer svg

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,6 @@
-import globeStats from "./globeStats.png";
 import dude from "./dude.png";
 import landingImage1 from "./drugs-and-barcode.png";
-import landingImage2 from "./drugs-and-barcode-2.png";
+import landingImage2 from "./drug-in-hand.png";
+import rightArc from "./landing-page-arc.png";
 
-export { globeStats, dude, landingImage1, landingImage2 };
+export { dude, landingImage1, landingImage2, rightArc };
--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -2,14 +2,15 @@ import { ReactComponent as WarningIcon } from "./warningIcon.svg";
 import { ReactComponent as SheildIcon } from "./sheildIcon.svg";
 import { ReactComponent as FullLogoIcon } from "./logo.svg";
 import { ReactComponent as HalfLogoIcon } from "./half-logo.svg";
-import MicroChipSvg from "./micro-chip.svg";
-import DrugsSvg from "./drugs.svg";
 import { ReactComponent as HowItWorksIconA } from "./howItWorksIconA.svg";
 import { ReactComponent as HowItWorksIconB } from "./howItWorksIconB.svg";
 import { ReactComponent as HowItWorksIconC } from "./howItWorksIconC.svg";
 import { ReactComponent as HowItWorksIconD } from "./howItWorksIconD.svg";
 import { ReactComponent as TboltIcon } from "./tBolt.svg";
 import { ReactComponent as CloseIcon } from "./close.svg";
+import MicroChipSvg from "./micro-chip.svg";
+import DrugsSvg from "./drugs.svg";
+import globeStats from "./globe-stats-2.svg";
 
 export {
   WarningIcon,
@@ -18,6 +19,7 @@ export {
   HalfLogoIcon,
   MicroChipSvg,
   DrugsSvg,
+  globeStats,
   HowItWorksIconA,
   HowItWorksIconB,
   HowItWorksIconC,
