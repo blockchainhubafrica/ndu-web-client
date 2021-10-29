25650f889312eb08dbd8847061f3b02ccbea2c49
davien21 <chidiebereekennia@gmail.com>
2021-10-29 18:45:13 +0100

    refactor: minor adjustment to functions

--- src/hooks/useShortenAddress.jsx
+++ src/hooks/useShortenAddress.jsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from "react";
 
+const largeCutText = (text) =>
+  `${text.substring(0, 3)}...${text.substring(text.length - 4)}`;
+
+const smallCutText = (text) =>
+  `${text.substring(0, 2)}...${text.substring(text.length - 2)}`;
+
 function useShortenAddress(address) {
   const [shortForm, setShortForm] = useState("*****");
 
   useEffect(() => {
-    if (window.innerWidth >= 768) {
-      setShortForm(
-        `${address.substring(0, 3)}...${address.substring(address.length - 4)}`
-      );
-    }
-
-    setShortForm(
-      `${address.substring(0, 2)}...${address.substring(address.length - 2)}`
-    );
+    if (window.innerWidth >= 768) setShortForm(largeCutText(address));
+    if (window.innerWidth < 768) setShortForm(smallCutText(address));
   }, [address]);
 
   return shortForm;
